c81f9612e4019ad33aae37f0d547550f45df0bf3
Lalet Scaria <laletscaria@yahoo.co.in>
2017-04-20 12:26:12 -0400

    Commented unused functions

--- cluster-spark-basic.py
+++ cluster-spark-basic.py
@@ -13,6 +13,7 @@ import sys
 import commands
 from time import gmtime, strftime
 import subprocess
+import shutil
 
 #Spark configuration
 conf = SparkConf() \
@@ -35,43 +36,43 @@ if args.d:
 
 #Function to compute ssd
 #Input is a pair of conditions and a common file
-def compute_ssd(x):
-    print "Enter:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    file_path_1 = root_dir+"/"+x[0]+"/"+x[2]+"/"+x[3]
-    file_path_2 = root_dir+"/"+x[1]+"/"+x[2]+"/"+x[3]
-    im1 = nibabel.load(file_path_1)
-    im2 = nibabel.load(file_path_2)
+#def compute_ssd(x):
+    #print "Enter:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    #file_path_1 = root_dir+"/"+x[0]+"/"+x[2]+"/"+x[3]
+    #file_path_2 = root_dir+"/"+x[1]+"/"+x[2]+"/"+x[3]
+    #im1 = nibabel.load(file_path_1)
+    #im2 = nibabel.load(file_path_2)
         
     # Check that both images have the same dimensions
-    shape1 = im1.header.get_data_shape()
-    shape2 = im2.header.get_data_shape()
-    if shape1 != shape2:
-        log_error("Images don't have the same shape!")
+    #shape1 = im1.header.get_data_shape()
+    #shape2 = im2.header.get_data_shape()
+    #if shape1 != shape2:
+        #log_error("Images don't have the same shape!")
 
-    data1 = im1.get_data()
-    data2 = im2.get_data()
+    #data1 = im1.get_data()
+    #data2 = im2.get_data()
     
-    xdim = shape1[0]
-    ydim = shape1[1]
-    zdim = shape1[2]
-    ssd=0
-    if len(shape1) == 4:
-      tdim = shape1[3]
+    #xdim = shape1[0]
+    #ydim = shape1[1]
+    #zdim = shape1[2]
+    #ssd=0
+    #if len(shape1) == 4:
+      #tdim = shape1[3]
       # Go through all the voxels and get the SSD
-      for x in range(0,xdim):
-            for y in range(0,ydim):
-              for z in range(0,zdim):
-                  for t in range(0,tdim):
-                      ssd += (data1[x][y][z][t]-data2[x][y][z][t])**2
-    else:
-      for x in range(0,xdim):
-          for y in range(0,ydim):
-              for z in range(0,zdim):
-                  ssd += (data1[x][y][z]-data2[x][y][z])**2
+      #for x in range(0,xdim):
+            #for y in range(0,ydim):
+              #for z in range(0,zdim):
+                  #for t in range(0,tdim):
+                      #ssd += (data1[x][y][z][t]-data2[x][y][z][t])**2
+    #else:
+      #for x in range(0,xdim):
+          #for y in range(0,ydim):
+              #for z in range(0,zdim):
+                  #ssd += (data1[x][y][z]-data2[x][y][z])**2
 
     # That's it!
-    print "Exit:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
-    return ssd
+    #print "Exit:",strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    #return ssd
 	
 #Compute mse
 def compute_mse(x):
@@ -82,6 +83,24 @@ def compute_mse(x):
    if return_value != 0:
      log_error(str(return_value)+" "+ str(output) +" "+"Command "+ "mse.sh" +" failed ("+command_string+").")
    return output
+
+
+#Mse using fslmaths
+#def compute_mse_sh(x):
+  #file_path_1 = root_dir+"/"+x[0]+"/"+x[2]+"/"+x[3]
+  #file_path_2 = root_dir+"/"+x[1]+"/"+x[2]+"/"+x[3]  
+  #diff="diff-XXXX.nii.gz"
+  #sqr="sqr-XXXX.nii.gz"
+  #diff=subprocess.check_output(["mktemp",diff])
+  #sqr=subprocess.check_output(["mktemp",sqr])
+  #print diff,sqr
+  #subprocess.call(["fslmaths",file_path_1,"-sub",file_path_2,"-sqr",diff])
+  #mean=subprocess.check_output(["fslstats",diff,"-m"])
+  #print mean
+  #os.remove(diff.replace("\n",""))
+  #os.remove(sqr.replace("\n",""))
+  #print "Removed folders"
+  #return mean.replace("\n","")
 
 #Log Error
 def log_error(message):
